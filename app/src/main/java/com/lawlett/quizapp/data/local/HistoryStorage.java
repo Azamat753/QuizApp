923709c65f8a408626f2a63d7c6e9bbe1b16f359
azamat753 <azamat.nazar99@gmail.com>
2020-03-12 19:35:29 +0600

    - HistoryDao написать query метода deleteAll - HistoryDao реализовать метод удаления по id - HistoryStorage создать методы для взаимодействия с Dao - Верстка Quiz, базовая верстка quiz по дизайну - Верстка Settings - Прописал доп запросы - Создал модельки для доп запросов

--- app/src/main/java/com/lawlett/quizapp/data/local/HistoryStorage.java
+++ app/src/main/java/com/lawlett/quizapp/data/local/HistoryStorage.java
@@ -1,6 +1,14 @@
 package com.lawlett.quizapp.data.local;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Query;
+
 import com.lawlett.quizapp.data.model.QuizResult;
+
+import java.util.List;
+
+//@Query("SELECT * FROM quiz_result WHERE id=:id")
+//    QuizResult get(int id);
 
 public class HistoryStorage {
     private HistoryDao mHistoryDao;
@@ -8,8 +16,6 @@ public class HistoryStorage {
     public HistoryStorage(HistoryDao historyDao) {
         this.mHistoryDao = historyDao;
     }
-
-
     void saveQuizResult(QuizResult quizResult) {
         mHistoryDao.insert(quizResult);
     }
@@ -20,5 +26,11 @@ public class HistoryStorage {
 
     void deleteAllQuizResult() {
         mHistoryDao.deleteAll();
+    }
+    void getQuizResultById (int id ) {
+        mHistoryDao.get(id);
+    }
+    public LiveData<List<QuizResult>>getAll(){
+        return mHistoryDao.getAll();
     }
 }
